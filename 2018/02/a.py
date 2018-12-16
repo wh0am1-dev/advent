8a7126c5875666c382e694ff5ec1afd993c6d1a7
neko250 <carlos.aguilar.112358@gmail.com>
2018-12-16 20:47:20 +0100

    feat: add 181202

---
+++ 2018/02/a.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+def rep(n, id):
+  for char in id:
+    if id.count(char) == n:
+      return 1
+
+  return 0
+
+
+if __name__ == '__main__':
+  twos = 0
+  threes = 0
+
+  with open('input.txt') as f:
+    data = f.read().splitlines()
+
+  for id in data:
+    twos += rep(2, id)
+    threes += rep(3, id)
+
+  print(twos * threes)
---
+++ 2018/02/b.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+def compare(a, b):
+  c = 0
+  d = ''
+
+  for i in range(len(a)):
+    if a[i] == b[i]:
+      d += a[i]
+    else:
+      c += 1
+
+  return (c, d)
+
+
+def search(data):
+  for i in range(len(data)):
+    for j in range(i + 1, len(data)):
+      c, d = compare(data[i], data[j])
+
+      if c == 1:
+        return d
+
+  return None
+
+
+if __name__ == '__main__':
+  with open('input.txt') as f:
+    data = f.read().splitlines()
+
+  print(search(data))
