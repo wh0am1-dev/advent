e56dfcc7c910fb7a027e3334f6d1c9690cc889bb
Neko250 <carlos.aguilar.112358@gmail.com>
2019-12-05 21:58:03 +0100

    feat: add segment and vec2 ops

--- 2019/03/Wire.js
+++ 2019/03/Wire.js
@@ -1,7 +1,26 @@
 class Vec2 {
-  constructor(x, y) {
+  constructor(x = 0, y = 0) {
     this.x = x
     this.y = y
+  }
+
+  mul(s = 1) {
+    return new Vec2(this.x * s, this.y * s)
+  }
+
+  add(v = new Vec2()) {
+    return new Vec2(this.x + v.x, this.y + v.y)
+  }
+}
+
+class Segment {
+  constructor(a = new Vec2(), b = new Vec2()) {
+    this.a = a
+    this.b = b
+  }
+
+  intersects(s = new Segment()) {
+    return false
   }
 }
 
@@ -14,7 +33,12 @@ const NORMALS = {
 
 class Wire {
   constructor(path) {
-    this.vertices = []
+    this.vertices = [new Vec2(0, 0)]
+
+    path.forEach(edge => {
+      const direction = edge.charAt(0)
+      const quantity = parseInt(edge.slice(1))
+    })
   }
 }
 
