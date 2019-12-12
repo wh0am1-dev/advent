fbfc8a3e31c10e21c833b26fb192dc90422e3d85
Neko250 <carlos.aguilar.112358@gmail.com>
2019-12-12 01:13:18 +0100

    chore: more wire

--- 2019/03/Wire.js
+++ 2019/03/Wire.js
@@ -1,29 +1,3 @@
-class Vec2 {
-  constructor(x = 0, y = 0) {
-    this.x = x
-    this.y = y
-  }
-
-  mul(s = 1) {
-    return new Vec2(this.x * s, this.y * s)
-  }
-
-  add(v = new Vec2()) {
-    return new Vec2(this.x + v.x, this.y + v.y)
-  }
-}
-
-class Segment {
-  constructor(a = new Vec2(), b = new Vec2()) {
-    this.a = a
-    this.b = b
-  }
-
-  intersects(s = new Segment()) {
-    return false
-  }
-}
-
 const NORMALS = {
   U: new Vec2(0, 1),
   R: new Vec2(1, 0),
@@ -31,15 +5,70 @@ const NORMALS = {
   L: new Vec2(-1, 0)
 }
 
+// ================================
+
 class Wire {
   constructor(path) {
     this.vertices = [new Vec2(0, 0)]
 
-    path.forEach(edge => {
-      const direction = edge.charAt(0)
-      const quantity = parseInt(edge.slice(1))
+    path.forEach(trans => {
+      const direction = trans.charAt(0)
+      const amount = parseInt(trans.slice(1))
+      const origin = this.vertices[this.vertices.length - 1]
+
+      this.vertices.push(origin.add(NORMALS[direction].mul(amount)))
     })
+
+    this.vertices.map((vert, i) => {
+      if (i >= this.vertices.length - 1) return null
+      return new Segment(vert, this.vertices[i + 1])
+    })
+
+    this.vertices.pop()
   }
 }
+
+// ================================
+
+class Segment {
+  constructor(a, b) {
+    this.a = a.x < b.x || a.y < b.y ? a : b
+    this.b = a.x < b.x || a.y < b.y ? b : a
+  }
+
+  get orientation() {
+    if (this.a.x === this.b.x) return 'v'
+    if (this.a.y === this.b.y) return 'h'
+    return undefined
+  }
+
+  intersects(s) {
+    if (this.orientation === s.orientation) return false
+
+    const sv = this.orientation === 'v' ? this : s
+    const sh = this.orientation === 'h' ? this : s
+
+    return false // TODO: implement
+  }
+}
+
+// ================================
+
+class Vec2 {
+  constructor(x = 0, y = 0) {
+    this.x = x
+    this.y = y
+  }
+
+  mul(s) {
+    return new Vec2(this.x * s, this.y * s)
+  }
+
+  add(v) {
+    return new Vec2(this.x + v.x, this.y + v.y)
+  }
+}
+
+// ================================
 
 module.exports = Wire
