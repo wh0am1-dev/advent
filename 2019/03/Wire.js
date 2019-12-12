42e8ecc0313088b953fab70846ae6122975929ff
Carlos Aguilar <carlos.aguilar@sequel.com>
2019-12-12 17:11:02 +0000

    feat: add 1903A solution

--- 2019/03/Wire.js
+++ 2019/03/Wire.js
@@ -1,59 +1,3 @@
-const NORMALS = {
-  U: new Vec2(0, 1),
-  R: new Vec2(1, 0),
-  D: new Vec2(0, -1),
-  L: new Vec2(-1, 0)
-}
-
-// ================================
-
-class Wire {
-  constructor(path) {
-    this.vertices = [new Vec2(0, 0)]
-
-    path.forEach(trans => {
-      const direction = trans.charAt(0)
-      const amount = parseInt(trans.slice(1))
-      const origin = this.vertices[this.vertices.length - 1]
-
-      this.vertices.push(origin.add(NORMALS[direction].mul(amount)))
-    })
-
-    this.vertices.map((vert, i) => {
-      if (i >= this.vertices.length - 1) return null
-      return new Segment(vert, this.vertices[i + 1])
-    })
-
-    this.vertices.pop()
-  }
-}
-
-// ================================
-
-class Segment {
-  constructor(a, b) {
-    this.a = a.x < b.x || a.y < b.y ? a : b
-    this.b = a.x < b.x || a.y < b.y ? b : a
-  }
-
-  get orientation() {
-    if (this.a.x === this.b.x) return 'v'
-    if (this.a.y === this.b.y) return 'h'
-    return undefined
-  }
-
-  intersects(s) {
-    if (this.orientation === s.orientation) return false
-
-    const sv = this.orientation === 'v' ? this : s
-    const sh = this.orientation === 'h' ? this : s
-
-    return false // TODO: implement
-  }
-}
-
-// ================================
-
 class Vec2 {
   constructor(x = 0, y = 0) {
     this.x = x
@@ -71,4 +15,69 @@ class Vec2 {
 
 // ================================
 
-module.exports = Wire
+const inRange = (x, a, b) => (x - a) * (x - b) <= 0
+
+const ORIENTATIONS = {
+  V: 'V',
+  H: 'H'
+}
+
+class Segment {
+  constructor(a, b) {
+    this.a = a.x < b.x || a.y < b.y ? a : b
+    this.b = a.x < b.x || a.y < b.y ? b : a
+  }
+
+  get orientation() {
+    if (this.a.x === this.b.x) return ORIENTATIONS.V
+    if (this.a.y === this.b.y) return ORIENTATIONS.H
+    return undefined
+  }
+
+  intersect(s) {
+    if (this.orientation === s.orientation) return undefined
+
+    const sv = this.orientation === ORIENTATIONS.V ? this : s
+    const sh = this.orientation === ORIENTATIONS.H ? this : s
+
+    // sh.a.x <= sv.a.x && sv.a.x <= sh.b.x && sv.a.y <= sh.a.y && sh.a.y <= sv.b.y
+    if (inRange(sv.a.x, sh.a.x, sh.b.x) && inRange(sh.a.y, sv.a.y, sv.b.y))
+      return new Vec2(sv.a.x, sh.a.y)
+
+    return undefined
+  }
+}
+
+// ================================
+
+const NORMALS = {
+  U: new Vec2(0, 1),
+  R: new Vec2(1, 0),
+  D: new Vec2(0, -1),
+  L: new Vec2(-1, 0)
+}
+
+class Wire {
+  constructor(path) {
+    this.vertices = [new Vec2(0, 0)]
+
+    path.forEach(trans => {
+      const direction = trans.charAt(0)
+      const amount = parseInt(trans.slice(1))
+      const origin = this.vertices[this.vertices.length - 1]
+
+      this.vertices.push(origin.add(NORMALS[direction].mul(amount)))
+    })
+
+    this.segments = this.vertices.map((vert, i) => {
+      if (i >= this.vertices.length - 1) return null
+      return new Segment(vert, this.vertices[i + 1])
+    })
+
+    this.segments.pop()
+  }
+}
+
+// ================================
+
+module.exports = { Wire, Segment, Vec2 }
--- 2019/03/a.js
+++ 2019/03/a.js
@@ -2,8 +2,24 @@
 
 const fs = require('fs')
 const path = require('path')
+const { Wire, Vec2 } = require('./Wire')
+
+const origin = new Vec2(0, 0)
+const manhattan = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
 
 const wires = fs
   .readFileSync(path.join(__dirname, 'input'), 'utf8')
   .split('\n')
   .map(wire => wire.split(','))
+  .map(wire => new Wire(wire))
+
+const intersections = []
+wires[0].segments.forEach(segmentA => {
+  wires[1].segments.forEach(segmentB => {
+    const p = segmentA.intersect(segmentB)
+    if (p) intersections.push(p)
+  })
+})
+
+const min = intersections.map(p => manhattan(p, origin)).reduce((acc, val) => Math.min(acc, val))
+console.log(min)
